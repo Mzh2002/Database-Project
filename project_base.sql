9cb00c01aaa9c527dbf8c9367439410d4099b546
Ruiyang Zhou <rz3zv@virginia.edu>
2024-10-14 19:58:21 -0400

    create all tables

--- project_base.sql
+++ project_base.sql
@@ -1,3 +1,11 @@
+-- Drop the database if it exists (to start over)
+IF DB_ID('Database_Project') IS NOT NULL
+BEGIN
+    DROP DATABASE Database_Project;
+END
+GO
+
+
 CREATE DATABASE Database_Project
 GO
 Use Database_Project
@@ -10,19 +18,22 @@ CREATE TABLE Users (
 );
 
 CREATE TABLE User_Preference(
-    FOREIGN KEY (user_id) REFERENCES Users(user_id),
+    user_id INT PRIMARY KEY,
     favorite_song_category VARCHAR(30),
+    FOREIGN KEY (user_id) REFERENCES Users(user_id),
 );
 
 CREATE TABLE Creator (
-    FOREIGN KEY (user_id) REFERENCES Users(user_id),
-    [level] INT
+    user_id INT PRIMARY KEY,
+    [level] INT,
+    FOREIGN KEY (user_id) REFERENCES Users(user_id),  
 );
 
 CREATE TABLE Listener (
-    FOREIGN KEY (user_id) REFERENCES Users(user_id),
+    user_id INT PRIMARY KEY,
     listen_time BIGINT,
     listen_num INT,
+    FOREIGN KEY (user_id) REFERENCES Users(user_id),
 );
 
 CREATE TABLE Song (
@@ -40,6 +51,7 @@ CREATE TABLE Singer (
 
 CREATE TABLE Song_List (
     song_list_id INT IDENTITY(1,1) PRIMARY KEY,
+    user_id INT,
     FOREIGN KEY (user_id) REFERENCES Users(user_id),
     list_name VARCHAR(30),
 );
@@ -47,4 +59,66 @@ CREATE TABLE Song_List (
 CREATE TABLE Album (
     album_id INT IDENTITY(1,1) PRIMARY KEY,
     [type] VARCHAR(10),
+);
+
+
+CREATE TABLE add_tag (
+    song_id INT,
+    speed FLOAT,
+    genre VARCHAR(50),
+    emotion VARCHAR(50),
+    PRIMARY KEY (song_id),
+    FOREIGN KEY (song_id) REFERENCES Song(song_id)
+);
+
+CREATE TABLE create_list (
+    song_list_id INT,
+    user_id INT,
+    tag VARCHAR(50),
+    description TEXT,
+    PRIMARY KEY (song_list_id, user_id),
+    FOREIGN KEY (song_list_id) REFERENCES Song_List(song_list_id),
+    FOREIGN KEY (user_id) REFERENCES Creator(user_id)
+);
+
+CREATE TABLE like_list (
+    user_id INT,
+    song_list_id INT,
+    rating INT CHECK (rating >= 0 AND rating <= 5),
+    PRIMARY KEY (user_id, song_list_id),
+    FOREIGN KEY (user_id) REFERENCES Listener(user_id),
+    FOREIGN KEY (song_list_id) REFERENCES Song_List(song_list_id)
+);
+
+CREATE TABLE sing (
+    song_id INT,
+    singer_id INT,
+    PRIMARY KEY (song_id, singer_id),
+    FOREIGN KEY (song_id) REFERENCES Song(song_id),
+    FOREIGN KEY (singer_id) REFERENCES Singer(singer_id)
+);
+
+CREATE TABLE contain (
+    song_list_id INT,
+    song_id INT,
+    PRIMARY KEY (song_list_id, song_id),
+    FOREIGN KEY (song_list_id) REFERENCES Song_List(song_list_id),
+    FOREIGN KEY (song_id) REFERENCES Song(song_id)
+);
+
+CREATE TABLE release (
+    singer_id INT,
+    album_id INT,
+    release_date DATE,
+    PRIMARY KEY (singer_id, album_id),
+    FOREIGN KEY (singer_id) REFERENCES Singer(singer_id),
+    FOREIGN KEY (album_id) REFERENCES Album(album_id)
+);
+
+CREATE TABLE include (
+    song_id INT,
+    album_id INT,
+    PRIMARY KEY (song_id, album_id),
+    FOREIGN KEY (song_id) REFERENCES Song(song_id),
+    FOREIGN KEY (album_id) REFERENCES Album(album_id)
 );
